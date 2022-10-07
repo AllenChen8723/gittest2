320f3eac0285e470311a8d45dec6f62b54f29fa5
allen <Allen0203.Chen@senao.com>
2022-10-07 10:40:53 +0800

    github pull test version2

---
+++ hellohello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>h
+#include <stdlib.h>
+
+
+
+int main(int argc, char const *argv[])
+{
+    int a =256;
+    printf("now is in the hellohello test_version2");
+
+    return 0;
+}
