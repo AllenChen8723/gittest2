5516fc333a10888f128a9d186d544035fcfc3a65
allen <Allen0203.Chen@senao.com>
2022-11-02 10:55:10 +0800

    update content v2

---
+++ gittest2_v2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>h
+#include <stdlib.h>
+
+
+
+int main(int argc, char const *argv[])
+{
+    int a =256;
+    printf("now is in the git_test_version3");
+
+    printf("now is in the git_test_version3");
+    
+ 
+    return 0;
+}
