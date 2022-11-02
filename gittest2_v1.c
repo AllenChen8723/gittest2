50fde7d5312b422e8243a564461d3a8449ebe093
allen <Allen0203.Chen@senao.com>
2022-11-02 10:37:14 +0800

    gittest2 update content

---
+++ gittest2_v1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>h
+#include <stdlib.h>
+
+
+
+int main(int argc, char const *argv[])
+{
+    int a =256;
+    printf("now is in the git_test_version2");
+
+    printf("now is in the git_test_version2");
+    
+ 
+    return 0;
+}
