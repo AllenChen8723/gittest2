127d17cd2b5ba13228ec4984ee765e1b34a315e5
allen <Allen0203.Chen@senao.com>
2022-11-02 11:02:54 +0800

    update

--- gittest2_v1.c
+++ gittest2_v1.c
@@ -7,7 +7,8 @@ int main(int argc, char const *argv[])
 {
     int a =256;
     printf("now is in the git_test_version2");
-
+    printf("now is in the git_test_version2");
+    printf("now is in the git_test_version2");
     printf("now is in the git_test_version2");
     
  
